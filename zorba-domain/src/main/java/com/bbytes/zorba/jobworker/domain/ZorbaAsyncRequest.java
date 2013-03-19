9e0ea1efdfa77e6f291396403677fea070504b05
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2013-03-19 21:21:12 +0530

    Zorba request response models for Async

---
+++ zorba-domain/src/main/java/com/bbytes/zorba/jobworker/domain/ZorbaAsyncRequest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 The Zorba Open Source Project 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.bbytes.zorba.jobworker.domain;
+
+/**
+ * Interface extension of {@link ZorbaRequest} for sending asynchronous request. 
+ *
+ * @author Dhanush Gopinath
+ *
+ * @version 0.0.1
+ */
+public interface ZorbaAsyncRequest extends ZorbaRequest {
+
+	/**
+	 * Sets the correlation id for the request
+	 * 
+	 * @param correlationId
+	 */
+	void setCorrelationId(String correlationId);
+	
+	/**
+	 * Returns the correlation id
+	 * 
+	 * @return
+	 */
+	String getCorrelationId();
+	
+	/**
+	 * Sets the reply queue
+	 * @param replyQueue
+	 */
+	void setReplyQueue(String replyQueue);
+	
+	/**
+	 * Returns the reply queue
+	 * @return
+	 */
+	String getReplyQueue();
+}
---
+++ zorba-domain/src/main/java/com/bbytes/zorba/jobworker/domain/impl/ZorbaAsyncResponseImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013 The Zorba Open Source Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.bbytes.zorba.jobworker.domain.impl;
+
+import com.bbytes.zorba.jobworker.domain.ZorbaAsyncResponse;
+
+/**
+ * Implementation fo {@link ZorbaAsyncResponse}
+ * 
+ * @author Dhanush Gopinath
+ * 
+ * @version
+ */
+public class ZorbaAsyncResponseImpl extends ZorbaResponseImpl implements ZorbaAsyncResponse {
+
+	private String correlationId;
+	private String replyQueue;
+
+	public String getCorrelationId() {
+		return correlationId;
+	}
+
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+
+	public String getReplyQueue() {
+		return replyQueue;
+	}
+
+	public void setReplyQueue(String replyQueue) {
+		this.replyQueue = replyQueue;
+	}
+
+}
